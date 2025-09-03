c855b0a2581ffbee3710ce26280ba4d53eec7fad
ZapAnton <wc3ft2@gmail.com>
2025-09-03 11:02:59 +0200

    Replaced the 'extractor' Java package with the Scala boilerplater

---
+++ src/main/scala/com/example/liftoff/extractor/email/ImapGmailExtractor.scala
@@ -0,0 +1,13 @@
+package com.example.liftoff.extractor.email
+
+import com.example.liftoff.extractor.ExtractorError
+import com.example.liftoff.storage.Storage
+
+class ImapGmailExtractor(emailAddress: String, userToken: String, storage: Storage) extends EmailExtractor(emailAddress: String, userToken: String, storage: Storage) {
+
+  override def authenticate(): Option[ExtractorError] = ???
+
+  override def close(): Option[ExtractorError] = ???
+
+  override def extract(): Option[ExtractorError] = ???
+}
